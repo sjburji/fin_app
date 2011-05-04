b73c52e2e5d5889878addc7bf8d3d35810614557
sjburji <supreethjburji@yahoo.com>
2011-05-04 18:51:29 +0530

    fin app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinApp::Application.config.secret_token = 'bbb1b13d18af0dd19e4f31a7933ee53caa4302d3df67f96a37b36536edc0c6170b240cf0a1802a9cea2cb86be3c76c5d8f682b962e496261bb656a8a762edce9'
